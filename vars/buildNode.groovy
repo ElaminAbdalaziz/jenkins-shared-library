cf1908b3d963df88c2db4d65ed927305e45313ac
Abdalaziz Elamin <131213410+ElaminAbdalaziz@users.noreply.github.com>
2026-08-12 05:54:26 +0300

    Update buildNode.groovy

--- vars/buildNode.groovy
+++ vars/buildNode.groovy
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 #!/user/bin/env groov
 def call(){
     echo "Building the application for branch $BRANCH_NAME"
     sh "npm install"
-=======
-#!/user/bin/env groov
-def call(){
-    echo "Building the application for branch $BRANCH_NAME"
-    sh "npm install"
->>>>>>> 6d99d25fb2f4127825a93cca57ce356883bf9b41
 }
