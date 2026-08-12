dc8c336a37f0300081e779f0e4bec4f5294756fc
Abdalaziz Elamin <131213410+ElaminAbdalaziz@users.noreply.github.com>
2026-08-12 05:57:14 +0300

    Update Docker.groovy

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/user/bin/env groovy
 package com.example
 
@@ -19,25 +18,3 @@ class Docker implements Serializable{
     }
 
 }
-=======
-#!/user/bin/env groovy
-package com.example
-
-class Docker implements Serializable{
-    def script
-
-    Docker(script){
-        this.script = script
-    }
-
-    def buildDockerImage(String imageName){
-        script.echo "Building the docker image..."
-        script.withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "docker build -t $imageName . "
-            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
-            script.sh "docker push $imageName"
-        }
-    }
-
-}
->>>>>>> 6d99d25fb2f4127825a93cca57ce356883bf9b41
