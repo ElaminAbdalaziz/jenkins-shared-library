14e6680ecb5d0dfeaa23d1e1e8681e3c873bf62b
Abdalaziz Elamin <131213410+ElaminAbdalaziz@users.noreply.github.com>
2026-08-12 05:55:43 +0300

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 #!/user/bin/env groovy
 import com.example.Docker
 
 def call(String imageName){
     return new Docker(this).buildDockerImage(imageName)
-=======
-#!/user/bin/env groovy
-import com.example.Docker
-
-def call(String imageName){
-    return new Docker(this).buildDockerImage(imageName)
->>>>>>> 6d99d25fb2f4127825a93cca57ce356883bf9b41
 }
